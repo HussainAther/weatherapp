8a829ec40dea92b1b1ae45244ac0a8a34fff25f1
Syed Hussain Ather <shussainather@gmail.com>
2023-07-06 18:11:16 -0400

    api key removed

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 // OpenWeather API endpoint for current weather and forecast
 const currentWeatherURL = 'https://api.openweathermap.org/data/2.5/onecall';
-const apiKey = 'e9e6a5ee0ae50574cbfee017b1d3741b';
+const apiKey = 'your_api_key';
 
 // Function to fetch weather data
 async function fetchWeatherData(lat, lon) {
